9808163a5691cef95396a295412ae121da0807e1
Christopher Nguyen <Chris_ngu@pm.me>
2020-02-24 20:39:05 -0600

    hashing passwords to database

--- enterprisesolution_fs/Backend/routes/users.js
+++ enterprisesolution_fs/Backend/routes/users.js
@@ -1,23 +1,43 @@
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
 let User = require('../models/user.model');
 
 //POST
 router.route('/').post((req, res) => {
     const username = req.body.username;
-    const password = req.body.password;
+    password = req.body.password;
 
-    const newUser = new User({ username, password });
+    bcrypt.hash(password, 10, function(err,hash){
+        password = hash;
+        const newUser = new User({ username, password});
+        if (err){
+            console.log(err);
+        }
 
-    newUser.save()
-    .then(() => res.json('New user added'))
-    .catch(err => res.status(400).json('Error: ' + err));
+        newUser.save()
+        .then(() => res.json('New user added'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    });
+
 });
+
+/*
+
+bcrypt.compare('somePassword', hash, function(err, res) {
+  if(res) {
+   // Passwords match
+  } else {
+   // Passwords don't match
+  } 
+});
+
+*/
 
 //GET
 router.route('/').get((req, res) => {
     User.find()
     .then(users => res.json(users))
     .catch(err => res.status(400).json('Error: ' + err));
- });
+    });
 
 module.exports = router;

b8b02c366039ab1f5a87a0967443310ed96687a5
Christopher Nguyen <Chris_ngu@pm.me>
2020-02-28 22:17:14 -0600

    creating landing page for login integration

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Button, Form, FormGroup, Label, Input} from 'reactstrap';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Form className="login-form">
+        <h1>
+          <span className="font-weight-bold">Enterprise</span>Solution
+        </h1>
+        <h2 className="text-center">Welcome</h2>
+        <FormGroup>
+          <label>Username</label>
+          <Input type="text" placeholder="Username"/>
+        </FormGroup>
+        <FormGroup>
+          <label>Password</label>
+          <Input type="password" placeholder="Password"/>
+        </FormGroup>
+        <Button className="btn-lg btn-dark btn-block">
+          Log in
+        </Button>
+        <div className="text-center pt-3 mb-3">
+        <a href="/register">Register</a>
+        <span className="p-2">|</span>
+        <a href="/forgot">Forgot Password</a>
+        </div>
+      </Form>
+    );
+  }
 }
 
 export default App;

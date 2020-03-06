e02758863d48efe581c6f85e175c997fa96853ac
Christopher Nguyen <Chris_ngu@pm.me>
2020-03-05 20:26:32 -0600

    adding missing component before passport

---
+++ src/components/Main_Screen.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { BrowserRouter as router, Route, link } from 'react-router-dom';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import Axios from 'axios';
+
+export default class MainScreen extends Component {
+    render(){
+        return(
+            <h1>Test here</h1>
+        )
+    }
+}

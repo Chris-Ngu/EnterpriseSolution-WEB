6c73f6b9c56639492ec3b11f10ed6374d51e6d85
Christopher Nguyen <Chris_ngu@pm.me>
2020-03-13 20:57:50 -0500

    adding routes for model, not added to server.js

--- backend/routes/project.js
+++ backend/routes/project.js
@@ -1,6 +1,27 @@
 const router = require('express').Router();
 let ProjectModel = require('../models/project.model');
 
+router.route('/').get((req, res) => {
+    ProjectModel.find()
+    .then(projects => res.json(projects))
+    .catch(err => res.status(400).json('Error: ' + err))
+});
+
 router.route('/add').post((req, res)=>{
-    
+    const projName = req.body.projName;
+    const description = req.body.description;
+    const members = req.body.members;
+    const startdate = req.body.startdate;
+    const finishdate = req.body.finishdate;
+
+    const newProject = new ProjectModel({
+        projName,
+        description,
+        members,
+        startdate,
+        finishdate
+    })
+    newProject.save()
+    .then(()=> res.json('New Project added'))
+    .catch(err => res.status(400).json('Error: ' + err));
 })

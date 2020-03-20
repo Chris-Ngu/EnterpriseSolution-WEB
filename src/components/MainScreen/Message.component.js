d139f507bff5f7656f512912d90ff52edc0a77fe
Christopher Nguyen <Chris_ngu@pm.me>
2020-03-19 21:29:54 -0500

    beginning work on socket.io integration

--- src/components/MainScreen/Message.component.js
+++ src/components/MainScreen/Message.component.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
+import { Form } from 'react-bootstrap';
+import { Input, Button } from 'reactstrap';
 
 export default class Message extends Component {
-    render(){
-        return(
-            <h1>Message test</h1>
+    render() {
+        return (
+            <body>
+                <div id="message-container">
+                    <Form id="send-container">
+                        <Input type="text" id="message-input" />
+                        <Button type="submit" id="send-button" className="btn-lg btn-dark btn-block">
+                            Send
+                        </Button>
+                    </Form>
+                </div>
+            </body>
         );
     }
 }
